9b09323cd2ddaa47bdd48e6a1280816030e1bfb5
napalm1970 <napalm1970@gmail.com>
2020-09-13 18:28:10 +0300

    added function

--- CLib.cpp
+++ CLib.cpp
@@ -22,3 +22,34 @@ int add(CStash* s, const void* element) {
   s->next++;
   return(s->next - 1);
 }
+
+void* fetch(CStash* s, int index) {
+  assert(0 <= index);
+  if(index >= s->next)
+    return 0;
+  return &(s->storage[index * s->size]);
+}
+
+int count(CStash* s) {
+  return s->next;
+}
+
+void inflate(CStash* s, int increase) {
+  assert(increase > 0);
+  int newQuantity = s->quantity + increase;
+  int newBytes = newQuantity * s->size;
+  int oldBytes = s->quantity * s->size;
+  unsigned char* b = new unsigned char[newBytes];
+  for(int i = 0; i < oldBytes; i++)
+    b[i] = s->storage[i];
+  delete [](s->storage);
+  s->storage = b;
+  s->quantity = newQuantity;
+}
+
+void cleanup(CStash* s) {
+  if(s->storage != 0) {
+    cout << "freeing storage" << endl;
+    delete []s->storage;
+  }
+}
