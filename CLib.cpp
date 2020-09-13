87fd4bb217c5ff7241de5641c59b82a427911e2a
napalm1970 <napalm1970@gmail.com>
2020-09-13 09:11:06 +0300

    Add CLib.cpp

---
+++ CLib.cpp
@@ -0,0 +1,24 @@
+#include "CLib.h"
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+const int increment = 100;
+
+void initialize(CStash* s, int sz) {
+  s->size = sz;
+  s->quantity = 0;
+  s->storage = 0;
+  s->next = 0;
+}
+
+int add(CStash* s, const void* element) {
+  if(s->next >= s->quantity)
+    inflate(s, increment);
+  int startBytes = s->next * s->size;
+  unsigned char* e = (unsigned char*) element;
+  for(int i = 0; i < s->size; i++)
+    s->storage[startBytes + i] = e[i];
+  s->next++;
+  return(s->next - 1);
+}
